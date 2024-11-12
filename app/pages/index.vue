78edf89f1f3eb1efe164c8b80af5923059fd3fcd
SoyaXy <3100034521@qq.com>
2024-11-12 09:10:44 +0800

    first

---
+++ app/pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <Suspense>
+      <ClientOnly>
+        <div text-gray:80>
+          You're offline
+        </div>
+      </ClientOnly>
+      <template #fallback>
+        <div italic op50>
+          <span animate-pulse>Loading...</span>
+        </div>
+      </template>
+    </Suspense>
+  </div>
+</template>
