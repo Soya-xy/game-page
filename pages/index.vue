294bc227a3dbf7425b367e6e864b1a43ba20b63e
soya <3100034521@qq.com>
2024-11-28 18:34:13 +0800

    fix: windows install

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
       <ClientOnly>
         <div class="container @container flex flex-col gap-y-[12px] mt-[12px] relative z-[20] sm:px-[24px]">
           <HomeSwiper />
-          <!-- <HomeBanner /> -->
+          <HomeBanner />
           <HomeRecent />
           <HomeCard />
           <BaseGameList title="Recommendar" />
@@ -19,7 +19,7 @@
         </div>
       </ClientOnly>
       <template #fallback>
-        <div class="italic" op50>
+        <div class="opacity-50 italic">
           <span class="animate-pulse">Loading...</span>
         </div>
       </template>
