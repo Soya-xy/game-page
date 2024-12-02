30fc31fb2e726316851a71a60242b924d9ddb6f2
soya <3100034521@qq.com>
2024-12-02 19:50:09 +0800

    feat: add page transition

--- layouts/wap.vue
+++ layouts/wap.vue
@@ -18,9 +18,23 @@ nuxtApp.hook('page:finish', () => {
     <div class="overflow-auto flex flex-col pb-[70px] bg-color pt-[48px] h-full">
       <main>
         <BaseSpin v-show="isPageLoading" is-page />
-        <slot />
+        <Transition name="page">
+          <slot />
+        </Transition>
       </main>
     </div>
     <LayoutWapTabbar />
   </div>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.3s ease-in-out;
+}
+
+.page-enter-from,
+.page-leave-to {
+  transform: translateX(100%);
+}
+</style>
