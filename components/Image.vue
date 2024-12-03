b2d2a6b2a751317ab5e5f4e4b3bf2a595a64db91
soya <3100034521@qq.com>
2024-12-03 19:08:59 +0800

    fix: add error image

--- components/Image.vue
+++ components/Image.vue
@@ -13,9 +13,18 @@ const isError = ref(false)
     :placeholder="img(src, { h: 10, f: 'png', blur: 2, q: 50 })" v-bind="$attrs"
     @error="isError = true"
   />
-  <NuxtImg
-    v-else src="/images/logo.avif" class="max-w-full h-full"
-    loading="lazy"
-    :placeholder="img('/images/logo.avif', { h: 10, f: 'png', blur: 2, q: 50 })"
-  />
+
+  <div
+    v-else
+    class="flex flex-col items-center justify-center gap-4 "
+  >
+    <NuxtImg
+      src="/images/afun/game_img_fial.png" class="w-[54%]"
+      loading="lazy"
+      :placeholder="img('/images/afun/game_img_fial.png', { h: 10, f: 'png', blur: 2, q: 50 })"
+    />
+    <p class="text-color text-sm">
+      Image is unavailable
+    </p>
+  </div>
 </template>
