ebf16257ade247112a8144063cf84038639e0620
soya <3100034521@qq.com>
2024-12-05 19:18:10 +0800

    feat: add some new component

---
+++ components/Category/Game.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const data = defineProp<any[]>([], true)
+</script>
+
+<template>
+  <div>
+    <div class="grid gap-[12px] grid-cols-[repeat(auto-fill,minmax(133px,1fr))]">
+      <div v-for="(game, index) in data" :key="index">
+        <BaseGameCard :info="game" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
