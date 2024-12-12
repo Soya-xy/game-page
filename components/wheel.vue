267d1f0ab80d1c715119f3e04d925bf4722c44dc
soya <3100034521@qq.com>
2024-12-12 23:25:50 +0800

    feat: add gachapon animation

---
+++ components/wheel.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div
+    data-v-d053f4f7="" data-v-2836fdb5-s=""
+    class="text-center flex flex-col justify-between absolute left-0 right-0 top-0 bottom-0 m-auto border-radius-1 bg-no-repeat bg-[length:100%] w-[520px] h-[422px] pt-[35px] px-[45px] pb-[37px]"
+    style="background-image: url(&quot;https://web-res-ccc.afunimg8.com/cdn-cgi/image/format=auto/C02/_E/activity/inviteWheel2/bg2.png&quot;);"
+  >
+    <img
+      data-v-d053f4f7="" data-v-2836fdb5-s=""
+      src="https://web-res-ccc.afunimg8.com/cdn-cgi/image/format=auto/C02/_E/activity/inviteWheel2/award_title.png?t=20241121"
+      alt="" importance="auto" class="absolute left-0 right-0 mx-auto h-[99px] top-[-51px]" lazy=""
+    >
+    <div data-v-d053f4f7="" data-v-2836fdb5-s="">
+      <h3 data-v-d053f4f7="" data-v-2836fdb5-s="" class="font-bold text-e-linear-color-10 italic text-[26px]">
+        Get $100.00 for free
+      </h3><img
+        data-v-d053f4f7="" data-v-2836fdb5-s=""
+        src="https://web-res-ccc.afunimg8.com/cdn-cgi/image/format=auto/C02/_E/activity/inviteWheel2/award1.png" alt=""
+        importance="auto" class="-rotate-90 mx-auto h-[91px]" lazy=""
+      >
+      <div data-v-d053f4f7="" data-v-2836fdb5-s="" class="mt-[20px]">
+        <div
+          data-v-d053f4f7="" data-v-2836fdb5-s="" class="invite-wheel2-sum font-[800] relative text-[54px]"
+          data-text="$93.26"
+        >
+          $93.26
+        </div>
+        <p data-v-d053f4f7="" data-v-2836fdb5-s="" class="text-e-g-color-14 font-medium leading-[1] text-[24px]">
+          <span data-v-d053f4f7="" data-v-2836fdb5-s="" class="text-e-g-color-123">$6.74</span> more for withdrawal.
+        </p>
+      </div>
+    </div><button
+      data-v-d053f4f7="" data-v-2836fdb5-s=""
+      class="h-[69px] font-bold text-e-g-color-88 rounded-full bg-color-linear-24"
+    >
+      <h3 data-v-d053f4f7="" data-v-2836fdb5-s="" class="text-[21px]">
+        Convide amigos
+      </h3>
+      <h4 data-v-d053f4f7="" data-v-2836fdb5-s="" class="text-[15px]">
+        To help with withdrawals
+      </h4>
+    </button><i
+      data-v-d053f4f7="" data-v-2836fdb5-s=""
+      class="inline-block h-[max-content] w-[max-content] icon sysicon-new-clean-6 cursor-pointer text-color-alpha-white-05 absolute left-0 right-0 mx-auto translate-y-[100%] text-[40px] bottom-[-28px]"
+    />
+  </div>
+</template>
+
+<style></style>
---
+++ pages/bonus/gachapon.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <Gachapon />
+  </div>
+</template>
+
+<style>
+
+</style>
