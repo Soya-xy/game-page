210a88b1d4c47c19d3636d0164a8c5b84d4f83d0
soya <3100034521@qq.com>
2024-12-13 00:09:07 +0800

    feat: optimize

--- components/Base/Spin.vue
+++ components/Base/Spin.vue
@@ -4,44 +4,10 @@ const isPage = defineProp<boolean>(false)
 
 <template>
   <div
-    class="app-loading md:top-[60px]" :class="{
+    class="app-loading md:top-[60px] w-full h-full fixed z-[99] bg-color" :class="{
       'top-0 !z-[9999] left-0': !isPage,
     }"
   >
     <div class="spin" />
   </div>
 </template>
-
-<style scoped>
-.app-loading {
-  position: fixed;
-  z-index: 99;
-  background-color: var(--bc-bgColor);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 8px;
-  width: 100%;
-  height: 100%;
-}
-
-.spin {
-  width: 25px;
-  height: 25px;
-  border-radius: 50%;
-  border: 4px solid var(--bc-activeColor);
-  border-top-color: transparent;
-  animation: spin 1s linear infinite;
-}
-
-@keyframes spin {
-  0% {
-    transform: rotate(0deg);
-  }
-
-  100% {
-    transform: rotate(360deg);
-  }
-}
-</style>
