bbf46cccea307976009853e75c91f9be91cf2c33
y <xxx.com>
2024-12-13 14:09:08 +0800

    feat: add installation API and update install page
    - Created `asyncInstall` and `asyncInstallRecords` functions in `api/install/index.ts` for handling app download and download records.
    - Updated `install.vue` to integrate the new installation API, including a download function for Android.
    - Enhanced the UI with improved button interactions and layout adjustments for better user experience.

---
+++ api/install/index.ts
@@ -0,0 +1,20 @@
+/**
+ * 下载
+ */
+export async function asyncInstall(platform: string) {
+  return useNuxtApp().$clientApi('/help/app-versions/get-app', {
+    params: {
+      platform,
+    },
+  })
+}
+/**
+ * 下载记录
+ */
+export async function asyncInstallRecords(platform: string) {
+  return useNuxtApp().$clientApi('/help/app-download-records/create-download-records', {
+    params: {
+      platform,
+    },
+  })
+}
